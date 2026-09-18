d8482fb0a5d9270dd87591eeddbbbc7f79466444
Ben Reeves <breeves715@pm.me>
2026-09-18 11:11:38 -0400

    User selects action by index, and callback executes. Move prompt to its own function

--- game/game.lua
+++ game/game.lua
@@ -1,40 +1,67 @@
-require("data.action");
+--@class Game
+--@field [any] any
 
+require("data.action")
 
 local Game = {}
-Game.__index = Game;
+Game.__index = Game
+
+local function getInput()
+    io.write("> ")
+    local input = io.read()
+    io.write("\n")
+    return input
+end
 
 function Game.run()
+	local endGame = false
 
-    local options = {
-        Action:create("Thing", function()
-            io.write("Thing callback")
-        end),
-    }
-    
-    print ("game running")
+    --@type table<integer, action[]>
+	local options = {
+		Action:create("Thing", function()
+			io.write("Thing callback\n")
+		end),
+		Action:create("Exit", function()
+			io.write("Exiting...")
+			endGame = true
+		end),
+	}
 
-    local endGame = false;
+	print("game running")
 
-    while (not endGame) do
-       io.write("Choose Option:\n") 
-       for i,v in ipairs(options) do
-           io.write(i .. ". " .. v.label)
-       end
-       io.write("\n")
+	local choice = "-1"
+	local idx = tonumber(choice)
+	local selection = nil
+	local error = false
 
-       local choice = io.read();
+	while not endGame do
+        error = false
 
-       if (choice == "1" or choice == "explore") then
-           io.write("exploring more\n");
-       elseif (choice == "2" or choice == "exit") then
-            io.write("exiting...\n");
-            endGame = true;
-        else
-            io.write("Option unknown. Please try again\n")
-        end
+		io.write("\nChoose Option:\n")
+		for i, v in ipairs(options) do
+			io.write(i .. ". " .. v.label .. "\n")
+		end
+		io.write("\n")
 
-    end
+		choice = getInput()
+		idx = tonumber(choice)
+
+		if idx and idx >= 1 and idx <= #options then
+			selection = options[idx]
+
+			if selection ~= nil then
+				selection.callback()
+			else
+				error = true
+			end
+		else
+			error = true
+		end
+
+		if error then
+			io.write("Option not recognized. Please try again.\n")
+		end
+	end
 end
 
 return Game
